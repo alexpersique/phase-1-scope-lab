954bbff24b62e76b27b315f2a521a4519ff6f508
Alexander <alexdgenkin@gmail.com>
2024-02-09 21:42:48 +1100

    Fills index.js

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Write your solution in this file!
+
+
+var customerName = "bob";
+
+function upperCaseCustomerName (){
+
+  return customerName = customerName.toUpperCase();
+};
+
+// console.log (upperCaseCustomerName(customerName));
+
+function setBestCustomer () {
+    bestCustomer = "not bob";
+    return bestCustomer;
+}
+
+function overwriteBestCustomer () {
+   bestCustomer = "maybe bob";
+   return bestCustomer;
+}
+
+const leastFavoriteCustomer = "john"
+
+function changeLeastFavoriteCustomer () {
+    leastFavoriteCustomer = "bob";
+    return leastFavoriteCustomer;
+}
+
+
